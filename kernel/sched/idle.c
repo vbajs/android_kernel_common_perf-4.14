e9b7eed57e443a11a1c3e59429c47bf159bd7c8d
Sultan Alsawaf <sultan@kerneltoast.com>
2025-12-08 15:34:23 +0800

    qos: Only wake idle CPUs which are affected by a request change
    The pm_qos idle wake-up mechanism currently wakes up *all* idle CPUs when
    there's a pm_qos request change, instead of just the CPUs which are
    affected by the change. This is horribly suboptimal and increases power
    consumption by needlessly waking idled CPUs.
    
    Additionally, pm_qos may kick CPUs which aren't even idle, since
    wake_up_all_idle_cpus() only checks if a CPU is running the idle task,
    which says nothing about whether or not the CPU is really in an idle state.
    
    Optimize the pm_qos wake-ups by only sending IPIs to CPUs that are idle,
    and by using arch_send_wakeup_ipi_mask() instead of wake_up_if_idle()
    which is used under the hood in wake_up_all_idle_cpus(). Using IPI_WAKEUP
    instead of IPI_RESCHEDULE, which is what wake_up_if_idle() uses behind the
    scenes, has the benefit of doing zero work upon receipt of the IPI;
    IPI_WAKEUP is designed purely for sending an IPI without a payload.
    
    Determining which CPUs are idle is done efficiently with an atomic bitmask
    instead of using the wake_up_if_idle() API, which checks the CPU's runqueue
    in an RCU read-side critical section and under a spin lock. Not very
    efficient in comparison to a simple, atomic bitwise operation. A cpumask
    isn't needed for this because NR_CPUS is guaranteed to fit within a word.
    
    CPUs are marked as idle as soon as IRQs are disabled in the idle loop,
    since any IPI sent after that point will cause the CPU's idle attempt to
    immediately exit (like when executing the wfi instruction). CPUs are marked
    as not-idle as soon as they wake up in order to avoid sending redundant
    IPIs to CPUs that are already awake.
    
    Change-Id: I04c9e2bd9317357e16d8184a104fe603d0d2dab2
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -251,6 +251,8 @@ static void do_idle(void)
 			tick_nohz_idle_stop_tick_protected();
 			cpuhp_report_idle_dead();
 			arch_cpu_idle_dead();
+		} else {
+			cpuidle_set_idle_cpu(cpu);
 		}
 
 		local_irq_disable();
@@ -268,6 +270,7 @@ static void do_idle(void)
 		} else {
 			cpuidle_idle_call();
 		}
+		cpuidle_clear_idle_cpu(cpu);
 		arch_cpu_idle_exit();
 	}
 
